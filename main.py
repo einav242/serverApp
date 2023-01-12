5d094b4b01adb15f174473d00b63866655a7558d
e2402 <e24022997b@gmail.com>
2023-01-12 19:30:27 +0200

    AccessLevel

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import auth
 from fastapi import FastAPI, Depends, HTTPException
+from pyasn1.type.univ import Null
 from pyrebase import pyrebase
+from firebase_admin import db
 
 from auth import AuthHandler
-from schemas import AuthDetails
+from schemas import AuthDetails, Access, NewUser, User
+import flask
 
 firebaseConfig = {'apiKey': "AIzaSyDICkdJ1RMuG-CGyNM6xAtLWaQmk6Lnr88",
                   'authDomain': "bff-a2663.firebaseapp.com",
@@ -24,7 +27,9 @@ auth_handler = AuthHandler()
 users = []
 
 cred = credentials.Certificate("bff-a2663-firebase-adminsdk-3eq11-c1610635f2.json")
-firebase_admin.initialize_app(cred)
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://bff-a2663-default-rtdb.firebaseio.com/'
+})
 
 
 def loginToFire(email: str, password: str):
@@ -46,5 +51,35 @@ def login(auth_details: AuthDetails):
     user_id = loginToFire(auth_details.email, auth_details.password)
     if user_id == "error":
         raise HTTPException(status_code=401, detail='Invalid username and/or password')
-    token = auth_handler.encode_token(auth_details.email)
     return {'user_id': user_id}
+
+
+@app.post('/AccessLevel')
+def AccessLevel(access: Access):
+    if db.reference(access.type).child(access.uid).get() is not None:
+        return "true"
+    else:
+        return "false"
+
+
+# @app.post('/RegisterUser')
+# def create_user(new_user: NewUser):
+#     auth.create_user(email=new_user.email, password=new_user.password)
+#     userID = auth.get_user_by_email(new_user.email).uid
+#     db.reference("Users").child(userID).set({'name': new_user.name,
+#                                              'email': new_user.email,
+#                                              'type': new_user.type,
+#                                              'id': userID,
+#                                              'breed': new_user.breed,
+#                                              'color': new_user.color,
+#                                              'flag': new_user.flag,
+#                                              'image': None,
+#                                              'phone': new_user.phone,
+#                                              'username': new_user.username})
+#     return "true"
+
+
+
+
+
+
--- schemas.py
+++ schemas.py
@@ -4,3 +4,35 @@ from pydantic import BaseModel
 class AuthDetails(BaseModel):
     email: str
     password: str
+
+
+class Access(BaseModel):
+    type: str
+    uid: str
+
+
+class NewUser(BaseModel):
+    name: str
+    email: str
+    username: str
+    flag: str
+    phone: str
+    breed: str
+    color: str
+    type: str
+    image: str
+    password: str
+
+
+class User(BaseModel):
+    name: str
+    email: str
+    username: str
+    flag: str
+    id: str
+    phone: str
+    breed: str
+    color: str
+    type: str
+    image: str
+
